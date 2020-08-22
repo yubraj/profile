207db024bd8f71365660d884d40b90b9f6856e75
Yubaraj Dahal <sagardahal34@gmail.com>
2020-08-22 09:53:26 +0545

    Updates

---
+++ precache-manifest.71a84deedc2161c1a1e6448a832475bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368ecd976ddc3d827415ca0e19e9a9e1",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "92d4479ba41c1c8cc7ef",
+    "url": "/profile/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "ce818f271e672467c1b8",
+    "url": "/profile/static/js/2.050e1cbb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/profile/static/js/2.050e1cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d4479ba41c1c8cc7ef",
+    "url": "/profile/static/js/main.8fb1c259.chunk.js"
+  },
+  {
+    "revision": "b5a7d259590e62b51bcc",
+    "url": "/profile/static/js/runtime-main.3026efc4.js"
+  },
+  {
+    "revision": "25180206a7bfde07516f817b509dc052",
+    "url": "/profile/static/media/Resume2020.25180206.pdf"
+  },
+  {
+    "revision": "8d4afd9a113a8faadad80d04c9c7b4b0",
+    "url": "/profile/static/media/me.8d4afd9a.jpg"
+  }
+]);
