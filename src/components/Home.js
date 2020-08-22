b6e460ba7f018b70989c233513713c311a7f87a9
Yubaraj Dahal <sagardahal34@gmail.com>
2020-08-22 09:56:57 +0545

    content changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,14 @@ function Home() {
       <div className="columns">
         <div className="column is-1"></div>
         <p className="column">
-          abc
+        Software Development professional having 7+ years of experience who can analyze, design, develop and implement cost-effective, high quality, high performance and innovative technology solutions using Microsoft technology stack.
+Experience in working with C#, ASP.NET, Core 1 and 2, ASP.NET MVC, ADO.NET, Entity Framework, SQL Server, LINQ Azure Services, UML and XML.
+Proficient in front end development using HTML5, Angular/Angular JS, Bootstrap, JaveScript and jQuery.
+Design and develop solutions that streamline workflows and improve business capabilities.
+Excellent knowledge of Object Oriented Design and Development.
+Analyze client requirements, develop and test innovative applications.
+Excellent communication skill.
+Works effectively on an independent basis or as part of team. 
           <br />
           <br />
          abcd <br />
